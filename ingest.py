428ea5876048bac5c7483a6aeca4ac8b207aefe9
Bence <68920299+obence09@users.noreply.github.com>
2021-07-26 13:27:53 +0200

    Create ingest.py

---
+++ ingest.py
@@ -0,0 +1,25 @@
+#Ingesting Data
+'''Pulling the data and processing it'''
+
+import requests
+import psycopg2
+
+
+#you have to give your own credentials at user and password
+conn = psycopg2.connect(dbname = "test_python", user="postgres", password = "postgres")
+cur = conn.cursor()
+sql = "INSERT INTO transactions (txid, uid, amount) VALUES (%s, %s, %s)"
+
+#streaming 10 rows of data from API to postgresql database
+with requests.get("http://127.0.0.1:5000/very_large_request/10", stream=True) as request:
+    print(request.text)
+    buffer = ""
+    for chunk in request.iter_content(chunk_size=1):
+        if chunk.endswith(b'\n'):
+            t = eval(buffer)
+            print(t)
+            cur.execute(sql, (t[0], t[1],t[2]))
+            conn.commit( )
+            buffer=""
+        else:
+            buffer += chunk.decode()
