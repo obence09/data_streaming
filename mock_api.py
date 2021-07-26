b609d4e8b9c0c5403029aebc982710c083d1bce4
Bence <68920299+obence09@users.noreply.github.com>
2021-07-26 13:25:26 +0200

    Create mock_api.py

---
+++ mock_api.py
@@ -0,0 +1,27 @@
+#Simulated API that provides transactional data
+
+from flask import Flask, Response, stream_with_context
+import time
+import uuid
+import random
+
+APP = Flask(__name__)
+
+
+@APP.route("/very_large_request/<int:rowcount>", methods = ["GET"])
+def get_large_request(rowcount):
+    '''returns N row of data'''
+    def data_generator():
+        '''The generator of mock data'''
+        '''Simulating Transactions'''
+        for i in range(rowcount):
+            time.sleep(.1)
+            txid = uuid.uuid4()
+            print(txid)
+            uid = uuid.uuid4()
+            amount = round(random.uniform(-10000,10000),2)
+            yield f"('{txid}', '{uid}', {amount})\n"
+    return Response(stream_with_context(data_generator()))
+
+if __name__ == "__main__":
+    APP.run(debug=True)
